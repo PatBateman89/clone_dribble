5527e3362440a2f54063b4fb18bacc6ea7650020
PatBateman89 <feric89@gmail.com>
2018-09-24 10:41:43 +0900

    try try try again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,13 +6,13 @@ CarrierWave.configure do |config|
     # production
       config.fog_credentials = {
         provider:              'AWS',                            # required
-        aws_access_key_id:     ENV['S3_KEY_ID'],                 # required
-        aws_secret_access_key: ENV['S3_ACCESS_KEY'],             # required
-        region:                ENV['S3_REGION'],                 # optional, defaults to 'us-east-1'
+        aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                 # required
+        aws_secret_access_key: ENV["AWS_SECRET_KEY"],             # required
+        region:                ENV["S3_REGION"],                 # optional, defaults to 'us-east-1'
         # host:                  's3.example.com',               # optional, defaults to nil
         # endpoint:              'https://s3.example.com:8080'   # optional, defaults to nil
       }
-      config.fog_directory  = ENV['S3_BUCKET_NAME']              # required
+      config.fog_directory  = ENV["S3_BUCKET_NAME"]              # required
       config.fog_public     = false                              # optional, defaults to true
       # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
       config.fog_authenticated_url_expiration = 2.minute
