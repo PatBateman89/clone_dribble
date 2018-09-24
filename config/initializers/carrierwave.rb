ddccee10f35c1637b25bc414157f8d948bba41d8
PatBateman89 <feric89@gmail.com>
2018-09-24 10:29:54 +0900

    lets try this again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,17 @@
 require 'fog/aws'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_provider = 'fog/aws'                                # required
-    # production
-      config.fog_credentials = {
-        provider:              'AWS',                            # required
-        aws_access_key_id:     ENV['S3_KEY_ID'],                 # required
-        aws_secret_access_key: ENV['S3_ACCESS_KEY'],             # required
-        region:                ENV['S3_REGION'],                 # optional, defaults to 'us-east-1'
-        # host:                  's3.example.com',               # optional, defaults to nil
-        # endpoint:              'https://s3.example.com:8080'   # optional, defaults to nil
-      }
-      config.fog_directory  = ENV['S3_BUCKET_NAME']              # required
-      config.fog_public     = false                              # optional, defaults to true
-      # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-      config.fog_authenticated_url_expiration = 2.minute
-    else
-      config.root = "#{Rails.root}/uploads"
-  end
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     'xxx',                        # required unless using use_iam_profile
+    aws_secret_access_key: 'yyy',                        # required unless using use_iam_profile
+    use_iam_profile:       true,                         # optional, defaults to false
+    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    host:                  's3.example.com',             # optional, defaults to nil
+    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'name_of_bucket'                                      # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
