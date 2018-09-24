29ccb97b59128617639e54d3e53362f5ba89306b
PatBateman89 <feric89@gmail.com>
2018-09-24 10:33:12 +0900

    here' exit quite '

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,10 +6,10 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     'xxx',                        # required unless using use_iam_profile
     aws_secret_access_key: 'yyy',                        # required unless using use_iam_profile
-    use_iam_profile:       true,                         # optional, defaults to false
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    # use_iam_profile:       false,                         # optional, defaults to false
+    # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'name_of_bucket'                                      # required
   config.fog_public     = false                                                 # optional, defaults to true
